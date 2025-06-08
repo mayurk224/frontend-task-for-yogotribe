84fc32003a9e6a6492b4511cd48485527faf2af3
Mayur Kamble <mayurkamble0250@gmail.com>
2025-06-08 10:05:43 +0530

    refactor: update App component to fetch and display random facts; clean up index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [fact, setFact] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  // Function to fetch a random fact from the API
+  const fetchFact = async () => {
+    setLoading(true);
+    try {
+      // used async/await to handle the fetch request
+      const res = await fetch("https://api.api-ninjas.com/v1/facts", {
+        headers: {
+          "X-Api-Key": import.meta.env.VITE_API_NINJAS_KEY,
+        },
+      });
+
+      const data = await res.json();
+      if (Array.isArray(data) && data.length > 0) {
+        setFact(data[0].fact);
+      } else {
+        setFact("No fact found.");
+      }
+    } catch (err) {
+      setFact("Error fetching fact.");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="">
+      <h1>Random Facts</h1>
+      <button onClick={fetchFact} disabled={loading}>
+        {loading ? "Loading..." : "Get Fact"}
+      </button>
+      {/* Display the fetched fact */}
+      {fact && <p>{fact}</p>}
+    </div>
+  );
+};
 
-export default App
+export default App;
